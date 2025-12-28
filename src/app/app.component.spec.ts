572c6654d2790d667f6338d11578d3f01e82eff7
BalwantSandhu <bsingh3@unb.ca>
2025-12-28 19:55:12 -0400

    finished typing game

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'typing' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('typing');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { faker } from '@faker-js/faker';
+import { NgIf, NgForOf } from "@angular/common";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NgIf, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = faker.lorem.sentence();
+  enteredText = '';
+  solved = false;
+
+  onInput(value:string){
+    console.log(value);
+    if(value === this.randomText){
+      this.solved = true;
+    }else{
+      this.solved = false;
+    }
+    this.enteredText = value;
+  }
+
+  compare(randomLetter:string, enteredLetter: string){
+    if(!enteredLetter){
+      return 'pending';
+    }
+    
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
